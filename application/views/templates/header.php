611d4d247ad8833fb3b9e9ae49ea18efbd9cb35a
Rishi Koushal <rishikoushal93@gmail.com>
2018-09-25 00:00:09 +0530

    added crud functionality

---
+++ application/views/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<style>
+		.container{padding: 10px;}
+	</style>
+</head>
+<body>
